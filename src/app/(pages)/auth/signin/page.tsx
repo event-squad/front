5b3219d884562e3512e0b3a3d9a09af9a442ac52
felipe <felipe.dreis.monteiro@gmail.com>
2023-05-26 13:45:36 -0300

    feat: page login

--- src/app/(pages)/auth/signin/page.tsx
+++
@@ -1,9 +0,0 @@
-const Page = () => {
- return(
-  <>
-   <h1>login page</h1>
-  </>
- )
-}
-
-export default Page;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,20 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"], display: "swap" });
 
 export const metadata = {
-  title: 'Event app',
-  
-}
+  title: "Event app",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="pt-Br">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
