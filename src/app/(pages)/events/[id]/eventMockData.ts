172de15d993b8c5dd435ee0bd88aaccc723ad140
emanuelmarcolongo <emanuelmarcolongo@gmail.com>
2023-05-29 18:25:37 -0300

    feat: dynamic route for event[id] with mockData

---
+++ src/app/(pages)/events/[id]/eventMockData.ts
@@ -0,0 +1,49 @@
+const eventMockData = [
+    {id: 1, 
+    name: 'Evento 1',
+    startDate: new Date().toISOString,
+    endDate: new Date().toISOString,
+    description: 'Evento Muito legal!!',
+    adress: 'Rio de Janeiro',
+    status: 'active',
+    producer: '123 Produções',
+    createAt: new Date().toISOString,
+    updatedAt: new Date().toISOString
+},
+{id: 2, 
+    name: 'Evento 2',
+    startDate: new Date().toISOString,
+    endDate: new Date().toISOString,
+    description: 'Evento Muito legalddd!!',
+    adress: 'SP ',
+    status: 'active',
+    producer: '123 Produções',
+    createAt: new Date().toISOString,
+    updatedAt: new Date().toISOString
+},
+{id: 3, 
+    name: 'Evento 3',
+    startDate: new Date().toISOString,
+    endDate: new Date().toISOString,
+    description: 'Evento Muito legal!!123123',
+    adress: 'MG',
+    status: 'active',
+    producer: '123 Produções',
+    createAt: new Date().toISOString,
+    updatedAt: new Date().toISOString
+},
+{id: 4, 
+    name: 'Evento 4',
+    startDate: new Date().toISOString,
+    endDate: new Date().toISOString,
+    description: 'Evento Muito legal!!',
+    adress: 'Acre',
+    status: 'active',
+    producer: '123 Produções',
+    createAt: new Date().toISOString,
+    updatedAt: new Date().toISOString
+}
+]
+
+
+export default eventMockData;
