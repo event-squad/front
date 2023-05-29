56ba84eae498cee7ac5463e5af58814acbfea683
felipe <felipe.dreis.monteiro@gmail.com>
2023-05-29 19:48:01 -0300

    feat: lib shadcn installed

--- src/app/(pages)/auth/login/logo.tsx
+++
@@ -1,9 +0,0 @@
-const Logo = () => {
-  return (
-    <div className="flex justify-center items-center mx-auto m-1 bg-white w-24 h-24 rounded-full">
-      <h1>App logo</h1>
-    </div>
-  );
-};
-
-export default Logo;
---
+++ src/app/(pages)/auth/signin/login.tsx
@@ -0,0 +1,9 @@
+import { Input } from "@/app/components/ui/input";
+
+export default function Login() {
+  return (
+    <div>
+      <Input placeholder="email" className="bg-red-500"/>
+    </div>
+  );
+}
