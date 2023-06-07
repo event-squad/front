382f9a78b3960e02445d8a5deeae8e9aa98d04a5
felipe <felipe.dreis.monteiro@gmail.com>
2023-06-07 19:19:58 -0300

    feat: liked dynamicaly

--- src/app/(pages)/events/[id]/layout.tsx
+++ src/app/(pages)/events/[id]/layout.tsx
@@ -1,11 +1,5 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { EventLiked, eventDetails } from "@/app/types/event";
-import { EventContext } from "./context/eventContext";
-import Like from "@/app/api/like/like";
-import Events from "@/app/api/events/events";
-
 type Props = {
   children: React.ReactNode;
   footer: React.ReactNode;
@@ -15,18 +9,33 @@ type Props = {
   };
 };
 
+import { useEffect, useState } from "react";
+import { EventLiked, eventDetails } from "@/app/types/event";
+import { EventContext } from "./context/eventContext";
+import Like from "@/app/api/like/like";
+import Events from "@/app/api/events/events";
+
 const Layout = (props: Props) => {
   const [event, setEvent] = useState<eventDetails>();
+  const [likedEvents, setLikedEvents] = useState<EventLiked[]>();
   const [active, setActive] = useState(false);
-  let token = localStorage.getItem("token");
+  const [token, setToken] = useState<string | null>();
 
   useEffect(() => {
-    if (!token) return;
+    if (!token) return setToken(localStorage.getItem("token"));
     const event = Events.getEventById(Number(props.params.id));
+    const likes = Like.getLikes(token);
 
     event
       .then((res) => {
         setEvent(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    likes
+      .then((res) => {
+        setLikedEvents(res);
       })
       .catch((err) => {
         console.log(err);
@@ -39,22 +48,22 @@ const Layout = (props: Props) => {
     if (active) {
       await Like.dislikeEvent(token, event.id);
       const likes = await Like.getLikes(token);
-      isActive(likes);
+      setLikedEvents(likes);
     } else {
       await Like.likeEvent(token, event.id);
       const likes = await Like.getLikes(token);
-      isActive(likes);
+      setLikedEvents(likes);
     }
   };
 
-  const isActive = (likes: EventLiked[]) => {
-    if (!event) return setActive(false);
-    const filtered = likes.filter((e) => e.eventId === event.id)
+  useEffect(() => {
+    if (!event || !likedEvents) return;
+    const filtered = likedEvents.filter((e) => e.eventId === event.id);
 
     if (!filtered[0]) return setActive(false);
 
     return setActive(true);
-  }
+  }, [likedEvents]);
 
   return (
     <EventContext.Provider value={{ event, active, handleActive }}>
